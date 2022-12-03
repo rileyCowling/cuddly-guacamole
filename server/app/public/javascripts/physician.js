b988412efe269e298fcc35a953664e93c26609cd
ryanstancliffe <94711998+ryanstancliffe@users.noreply.github.com>
2022-12-03 11:56:24 -0700

    initial physician.js
    Currently doesnt work, all commented out

---
+++ server/app/public/javascripts/physician.js
@@ -0,0 +1,18 @@
+//add 150px to #account height when patient drop down is opened
+//remove "" when closed
+
+/*
+var users = document.getElementsByClassName("cell-1");
+
+for (i = 0; i < users.length; i++) {
+    users[i].addEventListener("click", function() {
+        document.getElementById("account").classList.toggle("open");
+        if (document.getElementById("account").classList.contains("open")) {
+            document.getElementById("account").style.height += 150;
+        }
+        else {
+            document.getElementById("account").style.height -= 150;
+        }
+    })
+}
+*/
