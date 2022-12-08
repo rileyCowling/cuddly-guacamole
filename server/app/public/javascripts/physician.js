bd91e1cc491265250d9385218627ae7b6fba6a75
ryanstancliffe <ryanstancliffe@email.arizona.edu>
2022-12-07 20:03:02 -0700

    Update physician.js

--- server/app/public/javascripts/physician.js
+++ server/app/public/javascripts/physician.js
@@ -21,82 +21,82 @@ $("#logoutButton").click(function(){
 });
     
     
-function displayGraph(day) {
-    window.alert("displayGraph function called ");
-    let txdata = {
-        email: "user2",
-    }
-    $.ajax({
-        url: '/patients/data',
-        method: 'POST',
-        headers: { 'x-auth' : window.localStorage.getItem("token") },
-        contentType: 'application/json',
-        data: JSON.stringify(txdata),
-        dataType: 'json'
-    })
-    .done(function (data, textStatus, jqXHR) {
+// function displayGraph(day) {
+//     window.alert("displayGraph function called ");
+//     let txdata = {
+//         email: "user2",
+//     }
+//     $.ajax({
+//         url: '/patients/data',
+//         method: 'POST',
+//         headers: { 'x-auth' : window.localStorage.getItem("token") },
+//         contentType: 'application/json',
+//         data: JSON.stringify(txdata),
+//         dataType: 'json'
+//     })
+//     .done(function (data, textStatus, jqXHR) {
         
-        window.alert("made it to done");
+//         window.alert("made it to done");
 
 
-        //This is taken from the account.html script I think
-        //basically we just need to get the patient that just got clicked on 
+//         //This is taken from the account.html script I think
+//         //basically we just need to get the patient that just got clicked on 
 
 
-        //Shouldnt happen but just incase
-        // if(data.bpm == 0) {
-        //     console.log("no choice yet");
-        //     return;
-        // }
+//         //Shouldnt happen but just incase
+//         // if(data.bpm == 0) {
+//         //     console.log("no choice yet");
+//         //     return;
+//         // }
 
-        // //Make chart area visible and code needed for Chart.js to function
-        // document.getElementById("graphContainer").style.display = "block";
-        // //bpm chart
-        // const ctx1 = document.getElementById('bpmGraph');
-        // bpmChart = new Chart(ctx1, {
-        //     type: 'line',
-        //     data: {
-        //         //can be replaced with times/values from board
-        //         labels: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'],
-        //         datasets: [{
-        //             label: 'Heart Rate',
-        //             data: data.bpm,
-        //             borderWidth: 1
-        //         }]
-        //     },
-        //     options: {
-        //         maintainAspectRatio: false,
-        //         scales: {
-        //             y: {
-        //                 beginAtZero: false
-        //             }
-        //         }
-        //     }
-        // });
-        // //oxy chart
-        // const ctx2 = document.getElementById('oxyGraph');
-        // oxyChart = new Chart(ctx2, {
-        //     type: 'line',
-        //     data: {
-        //         //can be replaced with times/values from board
-        //         labels: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'],
-        //         datasets: [{
-        //             label: 'Oxygen Saturation',
-        //             data: data.oxy,
-        //             borderWidth: 1
-        //         }]
-        //     },
-        //     options: {
-        //         maintainAspectRatio: false,
-        //         scales: {
-        //             y: {
-        //                 beginAtZero: false
-        //             }
-        //         }
-        //     }
-        // });
-    }); 
-}
+//         // //Make chart area visible and code needed for Chart.js to function
+//         // document.getElementById("graphContainer").style.display = "block";
+//         // //bpm chart
+//         // const ctx1 = document.getElementById('bpmGraph');
+//         // bpmChart = new Chart(ctx1, {
+//         //     type: 'line',
+//         //     data: {
+//         //         //can be replaced with times/values from board
+//         //         labels: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'],
+//         //         datasets: [{
+//         //             label: 'Heart Rate',
+//         //             data: data.bpm,
+//         //             borderWidth: 1
+//         //         }]
+//         //     },
+//         //     options: {
+//         //         maintainAspectRatio: false,
+//         //         scales: {
+//         //             y: {
+//         //                 beginAtZero: false
+//         //             }
+//         //         }
+//         //     }
+//         // });
+//         // //oxy chart
+//         // const ctx2 = document.getElementById('oxyGraph');
+//         // oxyChart = new Chart(ctx2, {
+//         //     type: 'line',
+//         //     data: {
+//         //         //can be replaced with times/values from board
+//         //         labels: ['12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', '9PM', '10PM', '11PM'],
+//         //         datasets: [{
+//         //             label: 'Oxygen Saturation',
+//         //             data: data.oxy,
+//         //             borderWidth: 1
+//         //         }]
+//         //     },
+//         //     options: {
+//         //         maintainAspectRatio: false,
+//         //         scales: {
+//         //             y: {
+//         //                 beginAtZero: false
+//         //             }
+//         //         }
+//         //     }
+//         // });
+//     }); 
+// }
     
 
 
